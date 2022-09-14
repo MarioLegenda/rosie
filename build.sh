692ef60c298373f2c276d8afecfed92fae51dbd6
Mario <marioskrlec222@gmail.com>
2022-09-14 20:15:19 +0200

    Added binaries

--- build.sh
+++ build.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-
-echo "Compiling - Windows - 64-bit"
-# 64-bit
-GOOS=windows GOARCH=amd64 go build -o bin/simulator64.exe
-echo "Compiling - Windows - 32-bit"
-# 32-bit
-GOOS=windows GOARCH=386 go build -o bin/simulator32.exe
-
 echo "Compiling - Mac - 64-bit"
 # 64-bit
-GOOS=darwin GOARCH=amd64 go build -o bin/simulator64
+GOOS=darwin GOARCH=amd64 go build -o bin/rosie64mac
 
 echo "Compiling - Linux - 64-bit"
 # 64-bit
-GOOS=linux GOARCH=amd64 go build -o bin/simulator64linux
+GOOS=linux GOARCH=amd64 go build -o bin/rosie64linux
 echo "Compiling - Linux - 32-bit"
 # 32-bit
-GOOS=linux GOARCH=386 go build -o bin/simulator32linux
+GOOS=linux GOARCH=386 go build -o bin/rosie32linux
+
+echo "Compiling - Native"
+go build -o bin/rosie
